1db50856826463ccadbfa9a8a93c68eb86ddf73b
Kaveenxz <kaveenhansithx@gmail.com>
2024-04-02 09:52:47 +0530

    added notes display

--- src/script.js
+++ src/script.js
@@ -1,9 +1,23 @@
 document.getElementById("myForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
+    event.preventDefault();
   });
 
-function handleClick() {
+  function handleClick() {
+    const title = document.getElementById("note-title").value;
+    const description = document.getElementById("note-description").value;
 
-    console.log("hello");
+    if (title.trim() !== "" && description.trim() !== "") {
+      const addedNote = document.getElementById("added-note");
+      const newNote = document.createElement("div");
 
-}
+      newNote.innerHTML = `
+        <h3>${title}</h3>
+        <p>${description}</p>
+      `;
+
+      addedNote.appendChild(newNote);
+
+      document.getElementById("note-title").value = "";
+      document.getElementById("note-description").value = "";
+    }
+  }
