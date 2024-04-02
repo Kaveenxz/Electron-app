297b9605b7a8f32c1fa442650d98d57118cf85a7
Kaveenxz <kaveenhansithx@gmail.com>
2024-04-02 09:03:28 +0530

    call a function with js

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+  });
+
 function handleClick() {
-    const noteTitle = document.getElementById("note-title").value;
-    const noteDesc = document.getElementById("note-description").value;
 
-    console.log(noteDesc, noteTitle);
+    console.log("hello");
 
-    // Send IPC message to the main process
-    require('electron').ipcRenderer.send('add-work', { title: noteTitle, description: noteDesc });
 }
